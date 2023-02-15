dda9d4350aeb3d66ff1a7d6e7ccc9c41d2c0417d
Tsumire1208 <utamiori20210117@gmail.com>
2023-02-16 00:11:18 +0900

    チャットルーム新規作成機能　実装

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   resources :list, only: [:new, :create, :edit, :update, :destroy]do
     resources :card, except: [:index]
   end
+  resources :rooms, only: [:new, :create]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_02_15_130031) do
+ActiveRecord::Schema.define(version: 2023_02_15_145450) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -29,6 +29,21 @@ ActiveRecord::Schema.define(version: 2023_02_15_130031) do
     t.index ["user_id"], name: "index_lists_on_user_id"
   end
 
+  create_table "room_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "room_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_room_users_on_room_id"
+    t.index ["user_id"], name: "index_room_users_on_user_id"
+  end
+
+  create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +59,6 @@ ActiveRecord::Schema.define(version: 2023_02_15_130031) do
 
   add_foreign_key "cards", "lists"
   add_foreign_key "lists", "users"
+  add_foreign_key "room_users", "rooms"
+  add_foreign_key "room_users", "users"
 end
