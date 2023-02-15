65330f2bb39dd18b42837071914932913b3d24e7
Tsumire1208 <utamiori20210117@gmail.com>
2023-02-15 22:28:24 +0900

    カード新規作成機能　実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'top/index'
   root to: "chats#index"
   resources :users, only: [:edit, :update]
-  resources :list, only: [:new, :create, :edit, :update, :destroy]
-
+  resources :list, only: [:new, :create, :edit, :update, :destroy]do
+    resources :card, only: [:new, :create, :show, :edit, :update, :destroy]
+  end
 end
